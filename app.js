81a9f65bb132e9bd8ad0e09d61ec2c2941bdfe48
Kyle McV <kylemcv@gmail.com>
2018-09-01 12:54:58 -0700

    fix heroku items

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const passport = require("passport");
 
 require("./config/passport")(passport);
 
+const path = require("path");
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 

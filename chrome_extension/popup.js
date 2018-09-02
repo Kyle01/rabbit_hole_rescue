b3e7716a112cfb3ca71211bbbc300041d5a320ef
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-09-01 18:19:10 -0700

    Begin adding login input fields to popup

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -15,22 +15,35 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    let stop = document.getElementById('login');
-    stop.addEventListener('click', function () {
-        window.open("rabbit-hole-rescue.herokuapp.com/signin", "_blank");
+    let login = document.getElementById('login');
+    login.addEventListener('click', function () {
+        let username = document.getElementById("username").innerHTML;
+        let password = document.getElementById("password").innerHTML;
+        console.log('username');
+        console.log('password');
+        // window.open("rabbit-hole-rescue.herokuapp.com/signin", "_blank");
     });
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-  let stop = document.getElementById("signup");
-  stop.addEventListener("click", function() {
-      window.open("rabbit-hole-rescue.herokuapp.com/signup", "_blank");
+  let signup = document.getElementById("signup");
+
+  signup.addEventListener("click", function() {
+      let username = document.getElementById("username").innerHTML;
+      let password = document.getElementById("password").innerHTML;
+      console.log('running signup');
+      console.log('username');
+      console.log('password');
+//   window.open("rabbit-hole-rescue.herokuapp.com/signup", "_blank");
   });
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-  let stop = document.getElementById("visualization");
-  stop.addEventListener("click", function() {
-      window.open("rabbit-hole-rescue.herokuapp.com/history", "_blank");
+  let visualization = document.getElementById("visualization");
+  visualization.addEventListener("click", function() {
+    window.open("rabbit-hole-rescue.herokuapp.com/history", "_blank");
   });
 });
+
+
+

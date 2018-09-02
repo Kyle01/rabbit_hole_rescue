b4c2d6c7f30205640ddbc7ca118f12cbbedcaadc
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-09-02 13:57:17 -0700

    Send username from login to background.js

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,3 +1,35 @@
+// Signup button opens Web application's signup page in new tab
+
+document.addEventListener("DOMContentLoaded", function() {
+  let signup = document.getElementById("signup");
+  let xhr = new XMLHttpRequest();
+
+  signup.addEventListener("click", function() {
+    window.open("rabbit-hole-rescue.herokuapp.com/signup", "_blank");
+  });
+});
+
+// Login button sends username to background.js on success 
+
+document.addEventListener('DOMContentLoaded', function () {
+    let login = document.getElementById('login');
+    let xhr = new XMLHttpRequest();
+    login.addEventListener('click', function () {
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+        xhr.open("POST", "http://localhost:5000/api/users/login/", false);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        let str = `username=${username}&password=${password}`;
+        
+        xhr.send(str);
+            
+        if (xhr.status === 200) {
+            chrome.runtime.sendMessage({sender: "login", username: username});
+        }
+    });
+});
+
+// Start button sends message to background.js to start recording session 
 
 document.addEventListener('DOMContentLoaded', function () {
     let start = document.getElementById('start');
@@ -6,6 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// Stop button reloads extension, stopping recording and clearing username 
 
 document.addEventListener('DOMContentLoaded', function () {
     let stop = document.getElementById('stop');
@@ -14,43 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    let login = document.getElementById('login');
-    let xhr = new XMLHttpRequest();
-    login.addEventListener('click', function () {
-        let username = document.getElementById("username").value;
-        let password = document.getElementById("password").value;
-        xhr.open("POST", "http://localhost:5000/api/users/login/", true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        let str = `username=${username}&password=${password}`;
-        console.log(xhr["status"]);
-        xhr.send(str);
-        
-        console.log(xhr);
-        console.log(xhr.status);
-        console.log(xhr.response);
-        console.log(xhr.responseText);
-        // window.open("rabbit-hole-rescue.herokuapp.com/signin", "_blank");
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  let signup = document.getElementById("signup");
-  let xhr = new XMLHttpRequest();
-
-  signup.addEventListener("click", function() {
-    //   let username = document.getElementById("username").value;
-    //   let password = document.getElementById("password").value;
-    //   xhr.open("POST", "http://localhost:5000/api/users/register/", true);
-    //   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    //   str = `username=${username}&password=${password}`;
-    //   xhr.send(str);
-    //   console.log(xhr);
-    //   console.log(xhr.status);
-
-    window.open("rabbit-hole-rescue.herokuapp.com/signup", "_blank");
-  });
-});
+// Visualization button opens visualization page in new tab 
 
 document.addEventListener("DOMContentLoaded", function() {
   let visualization = document.getElementById("visualization");
